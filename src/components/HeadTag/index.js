d151820afc2625a15a85920a8c0b14c372e346b5
dafinysouza <22901057+dafinysouza@users.noreply.github.com>
2021-01-27 17:51:14 -0300

    Meta tags

--- src/components/HeadTag/index.js
+++ src/components/HeadTag/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+// import Favicon from "../../image/favicon.ico";
 
 function HeadTag() {
     return (
@@ -8,7 +9,8 @@ function HeadTag() {
             <meta property="og:url" content="https://color-theory-quiz.dafinysouza.vercel.app/" />
             <meta property="og:title" content="Quiz: Teoria das cores" />
             <meta property="og:description" content="Teste seu conhecimento sobre como as cores funcionam na web!" />
-            <meta property="og:image" content="" />
+            <meta property="og:image" content="https://raw.githubusercontent.com/dafinysouza/color-theory-quiz/main/src/image/og-color.jpg" />
+            {/* <link rel="shortcut icon" href={Favicon} /> */}
             <title>Quiz: Teoria das cores</title>
         </Head>
     )
