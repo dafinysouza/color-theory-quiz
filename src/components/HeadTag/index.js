866dda3f64a4b25d41decc8a095b0aebacf0fe8c
dafinysouza <22901057+dafinysouza@users.noreply.github.com>
2021-01-29 02:07:56 -0300

    Favicon adicionado

--- src/components/HeadTag/index.js
+++ src/components/HeadTag/index.js
@@ -9,20 +9,21 @@ function HeadTag() {
       <meta name="title" content="Quiz: Teoria das cores" />
       <meta name="description" content="Teste seu conhecimento sobre como as cores funcionam na web!" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="shortcut icon" href="https://raw.githubusercontent.com/dafinysouza/color-theory-quiz/main/src/img/favicon.ico" />
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://color-theory-quiz.dafinysouza.vercel.app/" />
       <meta property="og:title" content="Quiz: Teoria das cores" />
       <meta property="og:description" content="Teste seu conhecimento sobre como as cores funcionam na web!" />
-      <meta property="og:image" content="https://raw.githubusercontent.com/dafinysouza/color-theory-quiz/main/src/image/og-color.jpg" />
+      <meta property="og:image" content="https://raw.githubusercontent.com/dafinysouza/color-theory-quiz/main/src/img/og-color.png" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://color-theory-quiz.dafinysouza.vercel.app/" />
       <meta property="twitter:title" content="Quiz: Teoria das cores" />
       <meta property="twitter:description" content="Teste seu conhecimento sobre como as cores funcionam na web!" />
-      <meta property="twitter:image" content="https://raw.githubusercontent.com/dafinysouza/color-theory-quiz/main/src/image/og-color.jpg" />
+      <meta property="twitter:image" content="https://raw.githubusercontent.com/dafinysouza/color-theory-quiz/main/src/img/og-color.png" />
 
       {/* Google Fonts */}
       <link rel="preconnect" href="https://fonts.gstatic.com" />
